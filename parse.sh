7d7b9d6e03e0b2e1eae1ee6417fe744e16967573
Karen Keegan <karen.keegan@moredun.ac.uk>
2023-07-27 11:15:57 +0100

    The R script and bash script for parsing concqatenated blast text files into R.

---
+++ parse.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#SBATCH --cpus-per-task=4
+#SBATCH --mail-user=karen.keegan@moredun.ac.uk
+#SBATCH --mail-type=END,FAIL
+
+
+source activate R_env
+
+Rscript parse_v1_1.R /mnt/shared/scratch/kkeegan/personal/extraction_exp/super_accuracy_BLAST/Barcode_1/barcode_1.txt.gz /mnt/shared/scratch/kkeegan/personal/extraction_exp/super_accuracy_BLAST/Barcode_1/barcode_1_parsed.txt 90 1E-12 5 200
