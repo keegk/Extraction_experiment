711f481846e8024d7937c6644151329ed152c1f0
keegk <97610519+keegk@users.noreply.github.com>
2023-07-28 09:44:38 +0100

    Update super_basecalling_SQK_RBK004.sh

--- super_basecalling_SQK_RBK004.sh
+++ super_basecalling_SQK_RBK004.sh
@@ -9,6 +9,6 @@
 export PATH=/home/kkeegan/projects/jhi/bioss/kkeegan_onttestdata/ont-guppy-cpu/bin:$PATH
 echo "Input path is $1"
 echo "Save path is $2"
-echo "number of threads is $3"
+echo "number of threads is $4"
 
 guppy_basecaller --input_path $1 --save_path $2 --recursive --config $3 --barcode_kits "SQK-RBK004" --trim_barcodes --cpu_threads_per_caller $4 --num_callers $5  
