f2e972fe9f94eab375f3465c7c21392d5c918d81
keegk <97610519+keegk@users.noreply.github.com>
2023-08-01 10:33:17 +0100

    Update blast_ncbi_outfmt7.sh

--- blast_ncbi_outfmt7.sh
+++ blast_ncbi_outfmt7.sh
@@ -13,4 +13,4 @@ export BLASTDB=$BLASTDB:/mnt/shared/apps/databases/ncbi/
 
 
 
-blastn -query $1 -db nt -out $2 -num_threads 12 -max_target_seqs 10 -outfmt "7 sscinames qacc sacc pident length mismatch gapopen qstart qend sstart send evalue bitscore staxids"  
+blastn -query $1 -db nt -out $2 -num_threads 12 -max_target_seqs 10 -outfmt "7 sscinames qacc sacc pident length mismatch gapopen slen qlen qstart qend sstart send evalue bitscore staxids"  
