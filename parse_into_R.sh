9344b90fd6bc129d36aaa6bb0e659b25c05b3c08
keegk <97610519+keegk@users.noreply.github.com>
2023-08-01 10:16:39 +0100

    Update parse_into_R.sh
    changed the name of the .R file to parsefilter_blasttable.R

--- parse_into_R.sh
+++ parse_into_R.sh
@@ -6,4 +6,4 @@
 
 source activate R_env
 
-Rscript parse_v1_1.R /mnt/shared/scratch/kkeegan/personal/extraction_exp/super_accuracy_BLAST/Barcode_1/barcode_1.txt.gz /mnt/shared/scratch/kkeegan/personal/extraction_exp/super_accuracy_BLAST/Barcode_1/barcode_1_parsed.txt 90 1E-12 5 200
+Rscript parsefilter_blasttable.R /mnt/shared/scratch/kkeegan/personal/extraction_exp/super_accuracy_BLAST/Barcode_1/barcode_1.txt.gz /mnt/shared/scratch/kkeegan/personal/extraction_exp/super_accuracy_BLAST/Barcode_1/barcode_1_parsed.txt 90 1E-12 5 200
